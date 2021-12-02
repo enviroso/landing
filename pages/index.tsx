8aa439f388a947880e16cfa9111182aae8c7f6a9
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-12-03 02:14:29 +0530

    add metadata

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Meta } from "~/components/partials/Meta";
 import { Footer } from "~/components/parts/Footer";
 import { Section1 } from "~/components/parts/Section1";
 import { Section2 } from "~/components/parts/Section2";
@@ -8,6 +9,13 @@ import { Section5 } from "~/components/parts/Section5";
 export default function Home() {
   return (
     <>
+      <Meta
+        title="Enviroso ━ A Social Platform for Environmental Activists"
+        description="Meet Enviroso, A platform built for environmental activists to network and organize cleanup missions."
+        keywords="enviornment, activist, network, pollution, cleanup, mission, organization, platform"
+        image="https://avatars.githubusercontent.com/u/95420495?s=200&v=4"
+        url="https://enviroso.netlify.app"
+      />
       <Section1 />
       <Section2 />
       <Section3 />
