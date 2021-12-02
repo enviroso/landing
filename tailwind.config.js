4ab181f49a12ee31e63b726b44d9c22c79cb872c
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-12-02 19:56:42 +0530

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: 'jit',
+  theme: {
+    colors: {
+      accent: "#5DC192",
+      black: "#141212",
+      white: "#FFFFFF",
+      whiteMuted: "#CACACA",
+      blue: "#217AFF",
+      orange: "#C16F5D",
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
